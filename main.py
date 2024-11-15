813fe904227c05ea77161c173b9fa29f7e43804c
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-11-15 11:15:48 +0100

    Arreglados bugs, añadido movimiento en X

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 class ActionKind(Enum):
-    Idle = 0
-    Attacking = 1
+    Walking = 0
+    Idle = 1
+    Jumping = 2
+    Attacking = 3
 
 def on_a_pressed():
     global is_atacking
     is_atacking = True
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
+def on_left_pressed():
+    global is_walking_backwards
+    is_walking_backwards = True
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_right_pressed():
+    global is_walking_forward
+    is_walking_forward = True
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+is_walking_backwards = False
+is_walking_forward = False
 is_atacking = False
 scene.set_background_color(1)
 reaper = sprites.create(assets.image("""
@@ -15,6 +29,8 @@ reaper = sprites.create(assets.image("""
 idling = animation.create_animation(ActionKind.Idle, 75)
 attacking = animation.create_animation(ActionKind.Attacking, 30)
 is_atacking = False
+is_walking_forward = False
+is_walking_backwards = False
 idling.add_animation_frame(assets.image("""
     ReaperIdle1
 """))
@@ -50,13 +66,18 @@ attacking.add_animation_frame(assets.image("""
 """))
 animation.attach_animation(reaper, idling)
 animation.attach_animation(reaper, attacking)
-animation.set_action(reaper, ActionKind.Idle)
 
 def on_update_interval():
-    global is_atacking
+    global is_atacking, is_walking_forward, is_walking_backwards
     if is_atacking:
         animation.set_action(reaper, ActionKind.Attacking)
     else:
         animation.set_action(reaper, ActionKind.Idle)
     is_atacking = False
+    if is_walking_forward:
+        controller.move_sprite(reaper, 150, 0)
+    if is_walking_backwards:
+        controller.move_sprite(reaper, 150, 0)
+    is_walking_forward = False
+    is_walking_backwards = False
 game.on_update_interval(100, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    is_atacking = true
-})
-let is_atacking = false
 class ActionKind {
+    static Walking: number
+    private ___Walking_is_set: boolean
+    private ___Walking: number
+    get Walking(): number {
+        return this.___Walking_is_set ? this.___Walking : ActionKind.Walking
+    }
+    set Walking(value: number) {
+        this.___Walking_is_set = true
+        this.___Walking = value
+    }
+    
     static Idle: number
     private ___Idle_is_set: boolean
     private ___Idle: number
@@ -12,6 +19,17 @@ class ActionKind {
     set Idle(value: number) {
         this.___Idle_is_set = true
         this.___Idle = value
+    }
+    
+    static Jumping: number
+    private ___Jumping_is_set: boolean
+    private ___Jumping: number
+    get Jumping(): number {
+        return this.___Jumping_is_set ? this.___Jumping : ActionKind.Jumping
+    }
+    set Jumping(value: number) {
+        this.___Jumping_is_set = true
+        this.___Jumping = value
     }
     
     static Attacking: number
@@ -26,36 +44,92 @@ class ActionKind {
     }
     
     public static __initActionKind() {
-        ActionKind.Idle = 0
-        ActionKind.Attacking = 1
+        ActionKind.Walking = 0
+        ActionKind.Idle = 1
+        ActionKind.Jumping = 2
+        ActionKind.Attacking = 3
     }
     
 }
+
 ActionKind.__initActionKind()
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    
+    is_atacking = true
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    
+    is_walking_backwards = true
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    
+    is_walking_forward = true
+})
+let is_walking_backwards = false
+let is_walking_forward = false
+let is_atacking = false
 scene.setBackgroundColor(1)
-let reaper = sprites.create(assets.image`ReaperIdle1`, SpriteKind.Player)
+let reaper = sprites.create(assets.image`
+    ReaperIdle1
+`, SpriteKind.Player)
 let idling = animation.createAnimation(ActionKind.Idle, 75)
 let attacking = animation.createAnimation(ActionKind.Attacking, 30)
 is_atacking = false
-idling.addAnimationFrame(assets.image`ReaperIdle1`)
-idling.addAnimationFrame(assets.image`ReaperIdle2`)
-idling.addAnimationFrame(assets.image`ReaperIdle3`)
-idling.addAnimationFrame(assets.image`ReaperIdle4`)
-attacking.addAnimationFrame(assets.image`ReaperAttack1`)
-attacking.addAnimationFrame(assets.image`ReaperAttack2`)
-attacking.addAnimationFrame(assets.image`ReaperAttack3`)
-attacking.addAnimationFrame(assets.image`ReaperAttack4`)
-attacking.addAnimationFrame(assets.image`ReaperAttack5`)
-attacking.addAnimationFrame(assets.image`ReaperAttack6`)
-attacking.addAnimationFrame(assets.image`ReaperAttack7`)
+is_walking_forward = false
+is_walking_backwards = false
+idling.addAnimationFrame(assets.image`
+    ReaperIdle1
+`)
+idling.addAnimationFrame(assets.image`
+    ReaperIdle2
+`)
+idling.addAnimationFrame(assets.image`
+    ReaperIdle3
+`)
+idling.addAnimationFrame(assets.image`
+    ReaperIdle4
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack1
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack2
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack3
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack4
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack5
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack6
+`)
+attacking.addAnimationFrame(assets.image`
+    ReaperAttack7
+`)
 animation.attachAnimation(reaper, idling)
 animation.attachAnimation(reaper, attacking)
-animation.setAction(reaper, ActionKind.Idle)
-game.onUpdateInterval(100, function () {
+game.onUpdateInterval(100, function on_update_interval() {
+    
     if (is_atacking) {
         animation.setAction(reaper, ActionKind.Attacking)
     } else {
         animation.setAction(reaper, ActionKind.Idle)
     }
+    
     is_atacking = false
+    if (is_walking_forward) {
+        controller.moveSprite(reaper, 150, 0)
+    }
+    
+    if (is_walking_backwards) {
+        controller.moveSprite(reaper, 150, 0)
+    }
+    
+    is_walking_forward = false
+    is_walking_backwards = false
 })
