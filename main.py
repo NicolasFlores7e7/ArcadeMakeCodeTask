719149fdbf91ae597f01363b739448d78e741b8f
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-11-18 17:26:20 +0100

    Tiles y movimiento mejorado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ class ActionKind(Enum):
     Walking = 0
     Idle = 1
     Jumping = 2
-    Attacking = 3
+    attacking = 3
 
 def on_a_pressed():
     global is_atacking
@@ -14,23 +14,16 @@ def on_left_pressed():
     is_walking_backwards = True
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
-def on_right_pressed():
-    global is_walking_forward
-    is_walking_forward = True
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
-
 is_walking_backwards = False
-is_walking_forward = False
 is_atacking = False
 scene.set_background_color(1)
 reaper = sprites.create(assets.image("""
     ReaperIdle1
 """), SpriteKind.player)
+reaper.set_position(23, 95)
+reaper.set_scale(0.8, ScaleAnchor.MIDDLE)
 idling = animation.create_animation(ActionKind.Idle, 75)
-attacking = animation.create_animation(ActionKind.Attacking, 30)
-is_atacking = False
-is_walking_forward = False
-is_walking_backwards = False
+attacking2 = animation.create_animation(ActionKind.attacking, 30)
 idling.add_animation_frame(assets.image("""
     ReaperIdle1
 """))
@@ -43,41 +36,27 @@ idling.add_animation_frame(assets.image("""
 idling.add_animation_frame(assets.image("""
     ReaperIdle4
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack1
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack2
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack3
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack4
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack5
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack6
 """))
-attacking.add_animation_frame(assets.image("""
+attacking2.add_animation_frame(assets.image("""
     ReaperAttack7
 """))
 animation.attach_animation(reaper, idling)
-animation.attach_animation(reaper, attacking)
-
-def on_update_interval():
-    global is_atacking, is_walking_forward, is_walking_backwards
-    if is_atacking:
-        animation.set_action(reaper, ActionKind.Attacking)
-    else:
-        animation.set_action(reaper, ActionKind.Idle)
-    is_atacking = False
-    if is_walking_forward:
-        controller.move_sprite(reaper, 150, 0)
-    if is_walking_backwards:
-        controller.move_sprite(reaper, 150, 0)
-    is_walking_forward = False
-    is_walking_backwards = False
-game.on_update_interval(100, on_update_interval)
+animation.attach_animation(reaper, attacking2)
+animation.set_action(reaper, ActionKind.Idle)
--- main.ts
+++ main.ts
@@ -1,135 +1,39 @@
-class ActionKind {
-    static Walking: number
-    private ___Walking_is_set: boolean
-    private ___Walking: number
-    get Walking(): number {
-        return this.___Walking_is_set ? this.___Walking : ActionKind.Walking
-    }
-    set Walking(value: number) {
-        this.___Walking_is_set = true
-        this.___Walking = value
-    }
-    
-    static Idle: number
-    private ___Idle_is_set: boolean
-    private ___Idle: number
-    get Idle(): number {
-        return this.___Idle_is_set ? this.___Idle : ActionKind.Idle
-    }
-    set Idle(value: number) {
-        this.___Idle_is_set = true
-        this.___Idle = value
-    }
-    
-    static Jumping: number
-    private ___Jumping_is_set: boolean
-    private ___Jumping: number
-    get Jumping(): number {
-        return this.___Jumping_is_set ? this.___Jumping : ActionKind.Jumping
-    }
-    set Jumping(value: number) {
-        this.___Jumping_is_set = true
-        this.___Jumping = value
-    }
-    
-    static Attacking: number
-    private ___Attacking_is_set: boolean
-    private ___Attacking: number
-    get Attacking(): number {
-        return this.___Attacking_is_set ? this.___Attacking : ActionKind.Attacking
-    }
-    set Attacking(value: number) {
-        this.___Attacking_is_set = true
-        this.___Attacking = value
-    }
-    
-    public static __initActionKind() {
-        ActionKind.Walking = 0
-        ActionKind.Idle = 1
-        ActionKind.Jumping = 2
-        ActionKind.Attacking = 3
-    }
-    
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping,
+    attacking
 }
-
-ActionKind.__initActionKind()
-
-controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
-    
-    is_atacking = true
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (reaper.vy == 0) {
+        reaper.vy = -200
+    }
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    
-    is_walking_backwards = true
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.gameOver(false)
 })
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    
-    is_walking_forward = true
-})
-let is_walking_backwards = false
-let is_walking_forward = false
-let is_atacking = false
+let reaper: Sprite = null
 scene.setBackgroundColor(1)
-let reaper = sprites.create(assets.image`
-    ReaperIdle1
-`, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level3`)
+reaper = sprites.create(assets.image`ReaperIdle1`, SpriteKind.Player)
+reaper.setPosition(24, 155)
+reaper.setScale(0.7, ScaleAnchor.Middle)
+reaper.ay = 500
+scene.cameraFollowSprite(reaper)
+controller.moveSprite(reaper, 150, 0)
 let idling = animation.createAnimation(ActionKind.Idle, 75)
-let attacking = animation.createAnimation(ActionKind.Attacking, 30)
-is_atacking = false
-is_walking_forward = false
-is_walking_backwards = false
-idling.addAnimationFrame(assets.image`
-    ReaperIdle1
-`)
-idling.addAnimationFrame(assets.image`
-    ReaperIdle2
-`)
-idling.addAnimationFrame(assets.image`
-    ReaperIdle3
-`)
-idling.addAnimationFrame(assets.image`
-    ReaperIdle4
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack1
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack2
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack3
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack4
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack5
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack6
-`)
-attacking.addAnimationFrame(assets.image`
-    ReaperAttack7
-`)
+let attacking = animation.createAnimation(ActionKind.attacking, 30)
+idling.addAnimationFrame(assets.image`ReaperIdle1`)
+idling.addAnimationFrame(assets.image`ReaperIdle2`)
+idling.addAnimationFrame(assets.image`ReaperIdle3`)
+idling.addAnimationFrame(assets.image`ReaperIdle4`)
+attacking.addAnimationFrame(assets.image`ReaperAttack1`)
+attacking.addAnimationFrame(assets.image`ReaperAttack2`)
+attacking.addAnimationFrame(assets.image`ReaperAttack3`)
+attacking.addAnimationFrame(assets.image`ReaperAttack4`)
+attacking.addAnimationFrame(assets.image`ReaperAttack5`)
+attacking.addAnimationFrame(assets.image`ReaperAttack6`)
+attacking.addAnimationFrame(assets.image`ReaperAttack7`)
 animation.attachAnimation(reaper, idling)
 animation.attachAnimation(reaper, attacking)
-game.onUpdateInterval(100, function on_update_interval() {
-    
-    if (is_atacking) {
-        animation.setAction(reaper, ActionKind.Attacking)
-    } else {
-        animation.setAction(reaper, ActionKind.Idle)
-    }
-    
-    is_atacking = false
-    if (is_walking_forward) {
-        controller.moveSprite(reaper, 150, 0)
-    }
-    
-    if (is_walking_backwards) {
-        controller.moveSprite(reaper, 150, 0)
-    }
-    
-    is_walking_forward = false
-    is_walking_backwards = false
-})
+animation.setAction(reaper, ActionKind.Idle)
